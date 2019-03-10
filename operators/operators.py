1f36a6562c74cf6816897d8db7b29e0f5ebf946b
Bal2018 <44124657+Bal2018@users.noreply.github.com>
2019-03-10 22:31:32 +0000

    Updated operator python3 problem and readpage with challenge pic link from github

--- operators/operators.py
+++ operators/operators.py
@@ -1,25 +1,8 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-# Complete the solve function below.
-def solve(meal_cost, tip_percent, tax_percent):
-    TipForMeal =(tip_percent / 100) * meal_cost
-    TaxForMeal = (tax_percent /100) * meal_cost
-    TotalForMeal = meal_cost + TipForMeal + TaxForMeal
-    return(TotalForMeal)    
-
+# hackerrank - Used Python 3
+#!/bin/python3 
 if __name__ == '__main__':
-    meal_cost = float(input())
-
-    tip_percent = int(input())
-
-    tax_percent = int(input())
-
-    Total = solve(meal_cost, tip_percent, tax_percent)
-    print(int(round(Total)))
-
+    a = int(input())
+    b = int(input())
+    print (a+b)
+    print(a-b)
+    print(a*b)
