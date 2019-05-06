4c418177304b8db7e20b08940cf9506a0b065ad1
Bal2018 <44124657+Bal2018@users.noreply.github.com>
2019-05-06 18:00:54 +0100

    Division/

---
+++ Division/Division.py
@@ -0,0 +1,8 @@
+# hackerrank - Used Python 3
+#!/bin/python3
+if __name__ == '__main__':
+    a = int(input())   #Calculate integer division
+    b = int(input())   #Calculate the float of the numbers 
+print(a//b)
+print(a/b)
+ 
---
+++ Division/README.md
@@ -0,0 +1,5 @@
+# hackerrank - Excerise 
+
+ ![Else If Problem - Hackkerank ]
+ <img src="https://github.com/Bal2018/hackerrank/blob/master/Images/py-division-English.png?raw=true">
+
