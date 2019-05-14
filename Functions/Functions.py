5c6136cff87bdde3cd1c352b477f80c3d6ebf915
Bal2018 <44124657+Bal2018@users.noreply.github.com>
2019-05-14 23:09:56 +0100

    Writing a functin to check Leap year

---
+++ Functions/Functions.py
@@ -0,0 +1,8 @@
+# hackerrank - Used Python 3  Final Code
+#!/bin/python3
+if __name__ == '__main__':
+    n = int(input())    #User enters max number of lines
+for i in range(0,n):
+    print(i*i)          #Print out the square number of that line number
+
+
---
+++ Functions/README.md
@@ -0,0 +1,6 @@
+# hackerrank - Excerise 
+
+ ![Loop Problem - Hackkerank ] 
+ <img src="https://github.com/Bal2018/hackerrank/blob/master/Images/py-loops-English.png?raw=true">
+
+ 
