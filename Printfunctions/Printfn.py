f9cb8be2b705d22234c43c56ebdf0f13d8877cd7
Bal2018 <44124657+Bal2018@users.noreply.github.com>
2019-05-17 15:06:41 +0100

    Print a string of numbers

---
+++ Printfunctions/Printfn.py
@@ -0,0 +1,6 @@
+# hackerrank - Used Python 3  Final Code
+#!/bin/python3
+if __name__ == '__main__':
+    n = int(input())    #Enter in max numbers to enter
+for a in range (1,n+1):
+    print(a,end="")     #Print each number until max numbers reached
---
+++ Printfunctions/README.md
@@ -0,0 +1,5 @@
+# hackerrank - Excerise 
+
+ ![Print Function Problem - Hackkerank ] 
+ <img src="https://github.com/Bal2018/hackerrank/blob/master/Images/py-print-fn.png?raw=true">
+ 
