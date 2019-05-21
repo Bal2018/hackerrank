5a353f11e521c24dd4df25138074ed5cc352bd8f
Bal2018 <44124657+Bal2018@users.noreply.github.com>
2019-05-21 22:47:51 +0100

    Challenge to enter ages and output if within a valid range

---
+++ 30day-class/30day-class.py
@@ -0,0 +1,31 @@
+# hackerrank - Used Python 3 
+#!/bin/python3
+
+# Input: number of test cases to enter, actual numbers (age)
+# Output:  dependng upon age entered to outout if within or outside bounderies
+#!/bin/python3
+class Person:
+    def __init__(self,initialAge):
+        # Add some more code to run some checks on initialAge
+        if initialAge < 0:
+            print('Age is not valid, setting age to 0. ')
+            self.age = 0
+        else:
+            self.age = initialAge
+
+
+    def amIOld(self):
+        # Do some computations in here and print out the correct statement to the console
+        if self.age < 13:
+            print('You are young.')
+        elif self.age in range(13,18):
+            print('You are a teenager.')
+        else:
+            print('You are old.')
+
+
+    def yearPasses(self):
+        # Increment the age of the person in here
+        self.age = self.age + 1
+
+
